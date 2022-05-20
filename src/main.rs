da980c9241ebdda5290c301d1720bbc971dfdc8d
Mahadeer Mohamed <mahadeer2010@gmail.com>
2022-05-20 19:04:28 +0530

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate dotenv;
-
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use dotenv::dotenv;
+use std::{env, net::SocketAddr};
 
 mod handlers;
 mod models;
@@ -15,9 +15,10 @@ async fn main() -> std::io::Result<()> {
   let app_config = envy::prefixed("APP_")
     .from_env::<AppConfig>()
     .expect("Please provide port, host and workers values");
+  let address = SocketAddr::from(([127, 0, 0, 1], get_server_port()));
   let log_message = format!(
     "starting {} server at http://{}:{}",
-    app_config.application_name, app_config.host, app_config.port
+    app_config.application_name, address, app_config.port
   );
   log::info!("{}", log_message);
   HttpServer::new(|| {
@@ -26,8 +27,15 @@ async fn main() -> std::io::Result<()> {
       .default_service(web::to(fallback_handler))
       .wrap(Logger::new("%r"))
   })
-  .bind((app_config.host, app_config.port))?
+  .bind(address)?
   .workers(app_config.workers)
   .run()
   .await
+}
+
+fn get_server_port() -> u16 {
+  env::var("APP_PORT")
+    .ok()
+    .and_then(|port| port.parse().ok())
+    .unwrap_or_else(|| 8186)
 }
