4ba7b3f4e1b74cd19d2bd33d441e617721fdad59
Mahadeer Mohamed <mahadeer2010@gmail.com>
2022-05-22 13:08:07 +0530

    Chakra Redux added

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/base.css';
 import App from './components/App';
+import './styles/base.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('app')
 );
